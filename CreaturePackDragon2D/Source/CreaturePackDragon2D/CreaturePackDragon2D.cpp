af9f7bbee0d73258d3c447142f2183a00d242da3
kestrelm <creature@kestrelmoon.com>
2016-05-07 14:56:54 -0700

    New UE4 Dragon Demo using the CreaturePack Data Format

---
+++ CreaturePackDragon2D/Source/CreaturePackDragon2D/CreaturePackDragon2D.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CreaturePackDragon2D.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CreaturePackDragon2D, "CreaturePackDragon2D" );
---
+++ CreaturePackDragon2D/Source/CreaturePackDragon2D/CreaturePackDragon2DGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "CreaturePackDragon2DGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CREATUREPACKDRAGON2D_API ACreaturePackDragon2DGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
